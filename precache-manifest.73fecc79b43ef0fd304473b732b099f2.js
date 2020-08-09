76b5d9622cd5287a202e693a37dd39e218489b7f
EbrahemAbdraboh <ebrahem.abdraboh@gmail.com>
2020-08-09 02:14:21 +0200

    Updates

---
+++ precache-manifest.73fecc79b43ef0fd304473b732b099f2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a49df8b2b4016278df936b17091a22",
+    "url": "/arab/index.html"
+  },
+  {
+    "revision": "46601cb2c4839f00e80f",
+    "url": "/arab/static/css/2.382c35bb.chunk.css"
+  },
+  {
+    "revision": "5ca38f85498b79854bce",
+    "url": "/arab/static/css/main.0f769393.chunk.css"
+  },
+  {
+    "revision": "46601cb2c4839f00e80f",
+    "url": "/arab/static/js/2.5a413219.chunk.js"
+  },
+  {
+    "revision": "60882e4ef2630fd528dfb8fcf4b3b428",
+    "url": "/arab/static/js/2.5a413219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca38f85498b79854bce",
+    "url": "/arab/static/js/main.75973b02.chunk.js"
+  },
+  {
+    "revision": "75152aaeb6cd4d5d6e5f",
+    "url": "/arab/static/js/runtime-main.6dca5c8f.js"
+  },
+  {
+    "revision": "10c8bf831f37d6fc626e4e1976eb9322",
+    "url": "/arab/static/media/about-section.10c8bf83.png"
+  },
+  {
+    "revision": "87e417dbe16729b43aad91fca71fe773",
+    "url": "/arab/static/media/aboutSlide.87e417db.png"
+  },
+  {
+    "revision": "45197b00ff964510b2c571e72c4f4662",
+    "url": "/arab/static/media/contactSlide.45197b00.png"
+  },
+  {
+    "revision": "8b26bebfda572105a91bfe669e15d2fb",
+    "url": "/arab/static/media/homeSlide.8b26bebf.png"
+  },
+  {
+    "revision": "effe896431ad067984009429f5a8baaa",
+    "url": "/arab/static/media/inquirySlide.effe8964.png"
+  },
+  {
+    "revision": "44935da00e918922162b8b3b771511b1",
+    "url": "/arab/static/media/newsSlide.44935da0.png"
+  },
+  {
+    "revision": "38209df7b0701f5348b285058b78c5b0",
+    "url": "/arab/static/media/profile.38209df7.png"
+  },
+  {
+    "revision": "eca913ddf49b78c9a9f8d3379a6a0a31",
+    "url": "/arab/static/media/trainerImage.eca913dd.png"
+  },
+  {
+    "revision": "791bd80dc4de71b2b9e4f428775f0266",
+    "url": "/arab/static/media/training-section.791bd80d.png"
+  },
+  {
+    "revision": "7ac7ad1215dff684a49b7cb4e090c3a9",
+    "url": "/arab/static/media/why.7ac7ad12.png"
+  },
+  {
+    "revision": "9c3b95f6ff43fcc43309bc22751801f9",
+    "url": "/arab/static/media/whySection.9c3b95f6.png"
+  }
+]);
